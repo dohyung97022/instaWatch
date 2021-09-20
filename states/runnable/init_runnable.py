f0f235c9f5653849eede782fd9a6fbc3288a6af9
dohyung97022 <dohyung97022@gmail.com>
2021-09-20 23:12:14 +0900

    [#18] feature : init_state 에서 function 들로 행동단위 분리, 나중에 refactor 제대로 해야 합니다.

--- states/runnable/init_runnable.py
+++ states/runnable/init_runnable.py
@@ -1,11 +1,36 @@
 from ..Active import Active, State
 from modules.subprocess import sub_process
+from modules.subprocess.classes import firefox, ssh, registry
 
-firefox_location = r'C:\Program Files\Mozilla Firefox\firefox.exe'
-firefox_profile_option = '-P'
-firefox_process_name = 'firefox.exe'
+
+def execute_firefox(profile: str):
+    return sub_process.start(firefox.execute_cmd(profile))
+
+
+def connect_ssh(ip: str):
+    process = sub_process.start(ssh.connect_cmd(ip))
+    if not ssh.is_connected(process):
+        # TODO : 다른 exception 을 만들어 log 로 핸들하기
+        print('ssh is not connected')
+    return process
+
+
+def set_proxy_enable(enable: bool):
+    return sub_process.start(registry.InternetSetting.set_proxy_enable_cmd(enable))
+
+
+def set_proxy_server():
+    return sub_process.start(registry.InternetSetting.set_proxy_server_cmd())
 
 
 def run_init():
-    sub_process.start([firefox_location, firefox_profile_option, '1'])
+    processes = []
+    processes.append(execute_firefox('1'))
+    processes.append(connect_ssh('3.35.131.60'))
+    processes.append(set_proxy_enable(True))
+    processes.append(set_proxy_server())
+
+    for process in processes:
+        sub_process.kill(process)
+
     Active.state = State.trigger
