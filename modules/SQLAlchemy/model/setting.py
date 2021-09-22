5bf3cfe8e58e2a25d643460dce66a82cf339242b
dohyung97022 <dohyung97022@gmail.com>
2021-09-22 21:21:26 +0900

    [#31] style : rename account_run_loop_interval -> account_run_interval

--- modules/SQLAlchemy/model/setting.py
+++ modules/SQLAlchemy/model/setting.py
@@ -6,10 +6,10 @@ from datetime import time
 class Setting(Base):
     __tablename__ = "setting"
     id = Column(Integer, primary_key=True)
-    account_run_loop_interval = Column(Time)
+    account_run_interval = Column(Time)
     account_next_interval = Column(Time)
 
-    def __init__(self, email: str, account_run_loop_interval: time, account_next_interval: time):
+    def __init__(self, email: str, account_run_interval: time, account_next_interval: time):
         self.email = email
-        self.account_run_loop_interval = account_run_loop_interval
+        self.account_run_interval = account_run_interval
         self.account_next_interval = account_next_interval
