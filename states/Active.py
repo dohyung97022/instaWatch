0f2aa07e590eae54a1e9aa74defc255d2bac5728
dohyung97022 <dohyung97022@gmail.com>
2021-09-22 15:07:19 +0900

    [#27] feature : Active 클래스에 필요한 요소들 추가

--- states/Active.py
+++ states/Active.py
@@ -13,6 +13,20 @@ class State(enum.Enum):
 class Active:
     state = State.init
     account = ''
+    loop = 0
+    instance = None
     triggers = []
     events = []
     actions = []
+    processes = []
+
+    @classmethod
+    def reset(cls):
+        cls.state = State.init
+        cls.account = ''
+        cls.loop = 0
+        cls.instance = None
+        cls.triggers = []
+        cls.events = []
+        cls.actions = []
+        cls.processes = []
