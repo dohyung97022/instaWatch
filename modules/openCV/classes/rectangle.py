31d2a56f99bca52e05cc8ce9f548f1a39ca0b4ad
dohyung97022 <dohyung97022@gmail.com>
2021-10-23 17:59:21 +0900

    [#40] hotfix : opencv point 를 rectangle 으로 명칭 변경.

--- modules/openCV/classes/rectangle.py
+++ modules/openCV/classes/rectangle.py
@@ -1,4 +1,4 @@
-class Point:
+class Rectangle:
     x: int
     y: int
     x1: int
--- modules/openCV/open_cv.py
+++ modules/openCV/open_cv.py
@@ -1,20 +1,21 @@
 import cv2
 import numpy
-from .classes.point import Point
+from .classes.rectangle import Rectangle
 
 DETECT_DEFAULT_THRESHOLD = 0.9
 
 
 # 찾은 이미지들의 상하좌우 포인트를 돌려줍니다.
-def detect(detect_img_path: list[str], from_img_path: str, threshold: float = DETECT_DEFAULT_THRESHOLD) -> list[Point]:
+def detect(detect_img_path: list[str], from_img_path: str, threshold: float = DETECT_DEFAULT_THRESHOLD) \
+        -> list[Rectangle]:
     img_rgb = cv2.imread(from_img_path)
     img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
     template = cv2.imread(detect_img_path, 0)
     w, h = template.shape[::-1]
     res = cv2.matchTemplate(img_gray, template, cv2.TM_CCOEFF_NORMED)
     loc = numpy.where(res >= threshold)
-    res_point_list = []
+    res_rectangle_list = []
     for pt in zip(*loc[::-1]):
-        point = Point(pt[0], pt[1], pt[0] + w, pt[1] + h)
-        res_point_list.append(point)
-    return res_point_list
+        rectangle = Rectangle(pt[0], pt[1], pt[0] + w, pt[1] + h)
+        res_rectangle_list.append(rectangle)
+    return res_rectangle_list
